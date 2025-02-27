6d30f1cb29282e18ecc28b68a2c2aca062c4435f
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-02-26 23:34:04 -0300

    feat: add files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const jsonServer = require("json-server");
+
+const server = express();
+const router = jsonServer.router("db.json"); 
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server está rodando na porta ${PORT}`);
+});
